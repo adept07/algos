77234aace3004a81242715bcece212be0ef06c46
Satya Chekuri <satyavarma@gmail.com>
2018-12-29 04:40:57 -0800

    added DP and self temp vars

--- alogs/practice/fibonacci.js
+++ alogs/practice/fibonacci.js
@@ -22,15 +22,44 @@ function fibM(n) {
         return 1;
     }
     
-    if (fMap[n] !== undefined) {
-        return fMap[n];
-    } else {
+    if (fMap[n] === undefined) {
         fMap[n] = fibM(n-1) + fibM(n-2);
-        return fMap[n];
     }
+
+    return fMap[n];
 }
 
-//console.log(fibM(50));
+//console.log(fibM(55));
 //console.log(fMap);
 
-// WOW huge time difference between memoization and direct way.. 
+// dp - tabular
+function fibDP(n) {
+    var arr = [];
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (var i = 2; i <= n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    return arr[n]
+}
+console.log(fibDP(55));
+
+// mine - 2 temp vars
+function me(n) {
+    var pl_2 = 0,
+        pl_1 = 1,
+        ptemp = 0;
+        ptempPrev = 0;
+    for (var i = 2; i <= n; i++) {
+        ptemp = pl_1 + pl_2;
+        pl_2 = pl_1;
+        pl_1 = ptemp;
+    }
+    return ptemp;
+}
+console.log(me(55));
+
+// WOW huge time difference between memoization and direct recursion.. 
